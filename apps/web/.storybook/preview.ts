a13eb48bd35fd30235fb12a39272a89211000643
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-08-21 18:23:33 +0900

    🔨 formatOnSave로 인한 들여쓰기 문제 수정

--- apps/web/.storybook/preview.ts
+++ apps/web/.storybook/preview.ts
@@ -3,14 +3,14 @@ import "../src/app/globals.css";
 import type { Preview } from "@storybook/nextjs-vite";
 
 const preview: Preview = {
-    parameters: {
-        controls: {
-            matchers: {
-                color: /(background|color)$/i,
-                date: /Date$/i
-            }
-        }
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i
+      }
     }
+  }
 };
 
 export default preview;
