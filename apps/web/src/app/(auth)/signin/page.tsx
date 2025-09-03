8a8d35dc015509ee5657be03b8b01f9cd46de5ca
tare <siltarre@gmail.com>
2025-09-03 22:48:57 +0900

    ✨ (Refactor): 홈 페이지를 간소화하고, 로그인 및 사용자 정보를 SignInPage로 이동하여 인증 시스템 테스트 페이지 추가

--- apps/web/src/app/(auth)/signin/page.tsx
+++ apps/web/src/app/(auth)/signin/page.tsx
@@ -2,12 +2,18 @@
 
 import './styles.css';
 
+import { useAuth } from '@recipot/contexts';
+import { useRouter } from 'next/navigation';
+
 import { AuthButtons } from './_components/AuthButtons';
 import { IntroSlider } from './_components/IntroSlider';
 import { useIntroSlider } from './_components/useIntroSlider';
 
 export default function SignInPage() {
   const { current, handleSlideChange, intro } = useIntroSlider();
+  const { googleLogin, loading, login, logout, user } = useAuth();
+
+  const router = useRouter();
 
   return (
     <div className="mx-auto min-h-screen w-full">
@@ -16,7 +22,23 @@ export default function SignInPage() {
         current={current}
         onSlideChange={handleSlideChange}
       />
-      <AuthButtons />
+      {/* TODO: 테스트를 위한 로그인 */}
+      {loading && <div>Loading...</div>}
+      {user ? (
+        <>
+          <div>User: {user.name}</div>
+          <div>Email: {user.email}</div>
+          <div>Provider: {user.provider}</div>
+          <div>
+            <button onClick={logout}>Logout</button>
+            <button onClick={() => router.push('/signin/test')}>
+              테스트페이지
+            </button>
+          </div>
+        </>
+      ) : (
+        <AuthButtons kakaoLogin={login} googleLogin={googleLogin} />
+      )}
     </div>
   );
 }
