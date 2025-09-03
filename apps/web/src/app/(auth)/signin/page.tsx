8dbe2ff82bdc95d34ada2276a7298726ceba2760
tare <siltarre@gmail.com>
2025-09-03 23:03:05 +0900

    ✨ (Update): 개발 환경에서 사용자 정보를 표시하도록 SignInPage 수정, 로딩 상태 표시 개선

--- apps/web/src/app/(auth)/signin/page.tsx
+++ apps/web/src/app/(auth)/signin/page.tsx
@@ -23,9 +23,9 @@ export default function SignInPage() {
         onSlideChange={handleSlideChange}
       />
       {/* TODO: 테스트를 위한 로그인 */}
-      {loading && <div>Loading...</div>}
-      {user ? (
-        <>
+      {user && process.env.NODE_ENV === 'development' ? (
+        <div className="fixed top-0 left-1/2 z-50 flex w-full -translate-x-1/2 flex-col items-center gap-3 bg-white/60 px-6 pt-[10px] pb-[calc(10px+env(safe-area-inset-bottom))] backdrop-blur">
+          {loading && <div>Loading...</div>}
           <div>User: {user.name}</div>
           <div>Email: {user.email}</div>
           <div>Provider: {user.provider}</div>
@@ -35,10 +35,11 @@ export default function SignInPage() {
               테스트페이지
             </button>
           </div>
-        </>
+        </div>
       ) : (
         <AuthButtons kakaoLogin={login} googleLogin={googleLogin} />
       )}
+      <AuthButtons kakaoLogin={login} googleLogin={googleLogin} />
     </div>
   );
 }
