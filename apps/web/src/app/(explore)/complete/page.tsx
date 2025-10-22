dd5aff0bd3c4d9b5c495a23f0ce61efca5287c59
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-10-22 21:42:32 +0900

    ✨ 뱁새 이미지 추가, 재료 탐험 완료 페이지 추가

---
+++ apps/web/src/app/(explore)/complete/page.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import React from 'react';
+import Image from 'next/image';
+
+import RecipeHeader from '@/app/(recipeRecommend)/recipeRecommend/_components/RecipeHeader';
+import { Button } from '@/components/common/Button';
+import { Header } from '@/components/common/Header';
+import { CookIcon } from '@/components/Icons';
+
+import ExploreComplete from '../../../../public/explore.png';
+
+export default function ExploreCompletePage() {
+  return (
+    <>
+      <RecipeHeader onRefresh={() => {}} />
+      <Header.Spacer />
+      <div className="flex h-screen w-screen flex-col items-center justify-center">
+        <h2 className="text-22sb text-center text-gray-900">
+          재료 조합 탐험 완료!
+          <span className="text-24 ml-[2px]">&#x1F50D;</span>
+        </h2>
+        <Image
+          src={ExploreComplete}
+          alt="Explore Complete"
+          width={265}
+          height={271}
+          className="mt-[22.5px]"
+        />
+        <section className="mt-[34px]">
+          <h3 className="text-22sb text-center text-gray-900">
+            새로운 조합을 찾아 레시피를
+            <br />더 추천받아 보세요
+          </h3>
+        </section>
+        <div className="mt-[30px]">
+          <Button>
+            <CookIcon size={18} color="#ffffff" />
+            다시 선택하러 가기
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+}
--- apps/web/src/components/RecipeCard/RecipeActions.tsx
+++ apps/web/src/components/RecipeCard/RecipeActions.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useRouter } from 'next/navigation';
 
 import { Button } from '@/components/common/Button';
 import { CookIcon, HeartIcon } from '@/components/Icons';
@@ -12,9 +13,11 @@ export const RecipeActions = ({
   isLiked,
   onToggleLike,
 }: RecipeActionsProps) => {
-  // const handleToggleLike = useCallback(() => {
-  //   onToggleLike();
-  // }, [onToggleLike]);
+  const router = useRouter();
+
+  const handleExploreComplete = () => {
+    router.push('/complete');
+  };
 
   return (
     <div className="px-5 pb-6">
@@ -27,7 +30,10 @@ export const RecipeActions = ({
         >
           <HeartIcon className="h-5 w-5" color="#ffffff" active={isLiked} />
         </Button>
-        <Button className="h-[52px] flex-1 rounded-full bg-white text-gray-900">
+        <Button
+          onClick={handleExploreComplete}
+          className="h-[52px] flex-1 rounded-full bg-white text-gray-900"
+        >
           <CookIcon className="mr-2 h-[18px] w-[18px]" color="#212529" />
           <span className="text-17sb">요리하러 가기</span>
         </Button>
