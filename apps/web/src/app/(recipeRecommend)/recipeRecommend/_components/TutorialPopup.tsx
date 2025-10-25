6476677cbee5520df59d8ec92d0337a8037ecf6a
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-10-25 19:56:12 +0900

    💄 텍스트 및 버튼 텍스트 스타일 수정

--- apps/web/src/app/(recipeRecommend)/recipeRecommend/_components/TutorialPopup.tsx
+++ apps/web/src/app/(recipeRecommend)/recipeRecommend/_components/TutorialPopup.tsx
@@ -20,25 +20,25 @@ export default function TutorialPopup({ onClose }: TutorialPopupProps) {
       </div>
 
       {/* 튜토리얼 다이얼로그 */}
-      <div className="absolute top-[144px] left-1/2 z-[70] w-[342px] -translate-x-1/2 rounded-[18px] bg-white px-6 pb-6">
+      <div className="absolute top-[9rem] left-1/2 z-[70] w-[342px] -translate-x-1/2 rounded-[18px] bg-white px-6 pb-6">
         {/* 텍스트 영역 */}
         <div className="px-6 pt-[26px] pb-6 text-center">
-          <p className="text-base leading-6 text-[#212529]">
+          <p className="text-16b text-[#212529]">
             컨디션과 재료는 그대로,
             <br />
             다른 레시피를 원한다면?
             <br />
-            <span className="text-brand-primary text-16sb">
-              새로고침을 눌러 추천받으세요!{' '}
-            </span>
           </p>
+          <span className="text-17sb text-[#68982D]">
+            새로고침을 눌러 추천받으세요!{' '}
+          </span>
         </div>
 
         {/* 확인 버튼 */}
         <div className="flex h-6 justify-center">
           <button
             onClick={onClose}
-            className="ounded-full h-10 w-[294px] rounded-[100px] bg-[#68982d] px-5 py-2 text-center text-[15px] font-semibold text-white transition-colors hover:bg-[#5a7a26] focus:ring-2 focus:ring-[#68982d] focus:ring-offset-2 focus:outline-none"
+            className="text-15sb h-10 w-[294px] rounded-[100px] bg-[#68982d] px-5 py-2 text-center text-white transition-colors hover:bg-[#5a7a26] focus:ring-2 focus:ring-[#68982d] focus:ring-offset-2 focus:outline-none"
           >
             확인했어요
           </button>
