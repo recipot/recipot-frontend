d203fd75413e5471d33600330bf4354325a15863
twtwkim <alfog23@gmail.com>
2025-10-26 21:09:15 +0900

    빌드 에러 수정 포함

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,19 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   transpilePackages: ['@recipot/api'],
-  images: {
-    // 임시 이미지 허용
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'example.com',
-        pathname: '/**',
-      },
-    ],
-    domains: [
-      'appcook-bucket-cook-admin-ap-northeast-2.s3.ap-northeast-2.amazonaws.com',
-    ],
-  },
 
   // 개발 환경에서 백엔드 프록시 설정
   async rewrites() {
