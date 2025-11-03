4f26b99ddb4ce3d2aeb309cf48280998c5a88f77
twtwkim <alfog23@gmail.com>
2025-11-03 22:24:56 +0900

    test 에러 수정

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@recipot/api': path.resolve(__dirname, '../../packages/api/src'),
+      '@recipot/utils': path.resolve(__dirname, '../../packages/utils/src'),
     },
   },
   esbuild: {
