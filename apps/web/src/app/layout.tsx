60b3b86cfdcf2e0d3ff4d739831c474006b71633
tare <siltarre@gmail.com>
2025-11-04 21:44:41 +0900

    ✨ Add favicon and manifest files for improved branding and compatibility across devices

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -15,8 +15,23 @@ const pretendard = localFont({
 });
 
 export const metadata: Metadata = {
-  description: '냉장고 속 재료로 만드는 유연채식 집밥 레시피. 귀찮고 피곤한 날에도 건강한 한끼를 챙길 수 있게.',
+  description:
+    '냉장고 속 재료로 만드는 유연채식 집밥 레시피. 귀찮고 피곤한 날에도 건강한 한끼를 챙길 수 있게.',
   title: '작심삼일 없는 집밥 루틴 서비스. 한끼부터',
+};
+
+const gtmScript = `
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-NVSSS44G');
+`;
+
+const gtmScriptProps = { __html: gtmScript };
+const gtmNoscriptStyle: React.CSSProperties = {
+  display: 'none',
+  visibility: 'hidden',
 };
 
 export default function RootLayout({
@@ -27,6 +42,84 @@ export default function RootLayout({
   return (
     <html lang="ko" className={`${pretendard.variable} antialiased`}>
       <head>
+        {/* Favicons and Icons */}
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/favicon/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/favicon/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/favicon/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/favicon/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/favicon/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/favicon/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/favicon/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/favicon/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/favicon/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff" />
+
+        {/* <!-- Google Tag Manager --> */}
+        <script dangerouslySetInnerHTML={gtmScriptProps} />
+        {/* <!-- End Google Tag Manager --> */}
         <script
           src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.4/kakao.min.js"
           integrity="sha384-DKYJZ8NLiK8MN4/C5P2dtSmLQ4KwPaoqAfyA/DfmEc1VDxu4yyC7wy6K1Hs90nka"
@@ -35,6 +128,17 @@ export default function RootLayout({
         />
       </head>
       <body>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NVSSS44G"
+            height="0"
+            width="0"
+            style={gtmNoscriptStyle}
+            title="Google Tag Manager"
+          />
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         <Providers>{children}</Providers>
       </body>
     </html>
